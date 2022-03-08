a35c9ff0d149d14ae10b01b62aecb37784be59a0
Jarod <lee.jarod@gmail.com>
2022-03-08 09:59:01 +0800

    初始化noi2的脚本与资源

---
+++ docs/noi.sh
@@ -0,0 +1,9 @@
+sudo vim /etc/default/grub
+sudo update-grub
+timedatectl set-local-rtc 1 --adjust-system-clock
+sudo systemctl enable ssh
+wget https://gist.githubusercontent.com/jarodlee/75dfcb88111e4f5e9accc8e813d1ce0a/raw/89121fd0aebd58dba2c97cbe62f51476104144f4/.screenrc
+sudo mkdir /root/.ssh
+sudo wget https://ghtxx.cn/authorized_keys /root/.ssh/
+sudo chmod 600 /root/.ssh/authorized_keys
+sudo chown root:root /root/.ssh/authorized_keys
